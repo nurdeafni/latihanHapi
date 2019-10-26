ad3842557f03b7121be2ee6e6ae8980222b0e41d
nurdeafni <nurdeafni@gmail.com>
2019-10-25 23:12:42 -0700

    Assesment Selesai

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,97 +1,116 @@
-const Joi= require('@hapi/joi');
-const Models = require('../../models/index');
-const usersHandler= async (request, h) => {
-    try{
-        const user= await Models.User.findAll({})
+const Joi = require('@hapi/joi');
+const Models = require('../../models/index')
+const userHandler = async (request, h) => {
+    try {
+        const user = await Models.User.findAll({})
         return {
-            statusCode:200,
-            eror:'', 
-            massage:'Success',
-            data: user
+            statusCode: "200",
+            error: "",
+            message: "Succes",
+            content: user
         }
-    }catch(error){
-        return h.response({error: error.message}).code(400)
-       }
     }
+    catch (error) {
+        return h.response({ error: error.message }).code(400)
+    }
+}
 
-    const createUserHandler = async (request, h) => {
-        try{
-            const{ fristNameReq, lastNameReq, emailReq, genderReq, registerDateReq}= request.payload
-            console.log(request.payload);
-            const users= await Models.User.create({
-                firstName : fristNameReq,
-                lastName : lastNameReq,
-                email : emailReq,
-                gender: genderReq,
-                registerDate: registerDateReq
-            })
-            return{
-                statusCode:200,
-                eror:'', 
-                message: 'New user has been created.',
-                content: users
-                
-            }
-            }
-            catch (error){
-                return h.response({
-                    error: error.message
-                }).code(400)
+const createUserHandler = async (request, h) => {
+    try {
+        const { firstNameReq, lastNameReq, emailReq, genderReq, registerDateReq } = request.payload
+        console.log(request.payload);
+        const users = await Models.User.create({
+            firstName: firstNameReq,
+            lastName: lastNameReq,
+            email: emailReq,
+            gender: genderReq,
+            registerDate: registerDateReq,
+        })
+        return {
+            statusCode: "200",
+            error: "",
+            message: "Sukses terupload",
+            content: users
         }
     }
+    catch (error) {
+        return h.response({
+            error: error.message
+        }).code(400)
+    }
+}
 
-    const updateUserHandler = async (request, h) => {
-        try{
-            const user_id = request.params.id;
-            const{ fristNameReq, lastNameReq, emailReq,genderReq,registerDateReq} = request.payload;
-            const users = await Models.User.update({
-                firstName: fristNameReq,
-                lastName: lastNameReq,
-                email: emailReq,
-                gender: genderReq ,
-                registerDate: registerDateReq
-            }, {
-                where:{
-                    id: user_id
-                }
-            })
-            const dataRequest = request.payload
-            console.log('dataRequest');
-            console.log(users);
-            return{
-                data: dataRequest,
-                message: 'User has been updated'
+const patchUserHandler = async (request, h) => {
+    try {
+        const users_id = request.params.id;
+        const { firstNameReq, lastNameReq, emailReq, genderReq, registerDateReq } =
+            request.payload;
+        const users = await Models.User.update({
+            firstName: firstNameReq,
+            lastName: lastNameReq,
+            email: emailReq,
+            gender: genderReq,
+            registerDate: registerDateReq,
+        }, {
+            where: {
+                id: users_id
             }
-        }catch (error) {
-            return h.response({
-                error: error.message
-            }).code(400)
-        }
-    }
-    const deleteUserHandler = async (request, h) => {
-        try{
-            const user_id = request.params.id;
-            await Models.User.destroy({
-                where:{
-                    id: user_id
-                }
-            })
-            return {message: 'User has been deletred.'}
-        } catch (error){
-            return h.response({
-                error: error.message
-            }).code(400)
-        }
-    }
+        })
 
-module.exports =[
-    {method: 'GET',path: '/user/list',handler: usersHandler},
-    
+        const contentRequest = request.payload
+        console.log('contentRequest');
+        console.log(users);
+        return {
+            statusCode: "200",
+            error: "",
+            message: "Sukses terupdate",
+            content: contentRequest
+        }
+    }
+    catch (error) {
+        return h.response({
+            error: error.message
+        }).code(400)
+    }
+}
+
+const deleteUserHandler = async (request, h) => {
+    try {
+        const users_id = request.params.id;
+        await Models.User.destroy({
+            where: {
+                id: users_id
+            }
+        })
+        return { message: 'Data berhasil dihapus.' }
+    }
+    catch (error) {
+        return h.response({
+            error: error.message
+        }).code(400)
+    }
+}
+
+module.exports = [
+    { method: 'GET', path: '/user/list', handler: userHandler },
+
     {
         method: 'POST',
         path: '/user/postData',
+        config: {
+            validate: {
+                payload: {
+                    firstNameReq: Joi.string().required(),
+                    lastNameReq: Joi.string().required(),
+                    emailReq: Joi.string().email(),
+                    genderReq: Joi.string().required(),
+                    registerDateReq: Joi.date().required(),
+                }
+            }
+        },
         handler: createUserHandler
     },
-    {method: 'PATCH', path: '/user/postUpdate/{id}', handler: updateUserHandler},
-    {method: 'DELETE', path: '/user/deleteUser/{id}', handler: deleteUserHandler},
-]
+
+    { method: 'PATCH', path: '/user/postUpdate/{id}', handler: patchUserHandler },
+    { method: 'DELETE', path: '/user/deleteUser/{id}', handler: deleteUserHandler },
+];
